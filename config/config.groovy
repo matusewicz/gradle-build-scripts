3652249378e65e06e63bb6a0c1845194f2d51306
Jan Matusewicz <jan.matusewicz@icloud.com>
2017-07-23 11:32:51 +0200

    Enable environment specific build.

---
+++ config/config.groovy
@@ -0,0 +1,16 @@
+buildScriptsSubmodulePath = '.'
+
+environments {
+
+    development {
+        environment = 'development'
+    }
+
+    staging {
+        environment = 'staging'
+    }
+
+    production {
+        environment = 'production'
+    }
+}
